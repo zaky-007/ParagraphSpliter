f247fed0d0e59b34e9318007cff5bd86380afd7f
Xico <Xico@DESKTOP-GCT00GQ>
2025-11-07 10:02:09 +0200

    Add project files.

---
+++ ParagraphSpliter/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace ParagraphSpliter
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            inputTxt = new System.Windows.Forms.TextBox();
+            outPutTxt = new System.Windows.Forms.TextBox();
+            filterTxt = new System.Windows.Forms.TextBox();
+            label1 = new System.Windows.Forms.Label();
+            label2 = new System.Windows.Forms.Label();
+            label3 = new System.Windows.Forms.Label();
+            button1 = new System.Windows.Forms.Button();
+            SuspendLayout();
+            // 
+            // inputTxt
+            // 
+            inputTxt.AllowDrop = true;
+            inputTxt.Location = new System.Drawing.Point(12, 12);
+            inputTxt.Name = "inputTxt";
+            inputTxt.Size = new System.Drawing.Size(239, 23);
+            inputTxt.TabIndex = 0;
+            inputTxt.DragDrop += inputTxt_DragDrop;
+            inputTxt.DragEnter += inputTxt_DragEnter;
+            // 
+            // outPutTxt
+            // 
+            outPutTxt.AllowDrop = true;
+            outPutTxt.Location = new System.Drawing.Point(12, 41);
+            outPutTxt.Name = "outPutTxt";
+            outPutTxt.Size = new System.Drawing.Size(239, 23);
+            outPutTxt.TabIndex = 1;
+            outPutTxt.DragDrop += outPutTxt_DragDrop;
+            outPutTxt.DragEnter += outPutTxt_DragEnter;
+            // 
+            // filterTxt
+            // 
+            filterTxt.AllowDrop = true;
+            filterTxt.Location = new System.Drawing.Point(12, 70);
+            filterTxt.Name = "filterTxt";
+            filterTxt.Size = new System.Drawing.Size(239, 23);
+            filterTxt.TabIndex = 2;
+            filterTxt.DragDrop += filterTxt_DragDrop;
+            filterTxt.DragEnter += filterTxt_DragEnter;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new System.Drawing.Point(257, 15);
+            label1.Name = "label1";
+            label1.Size = new System.Drawing.Size(35, 15);
+            label1.TabIndex = 3;
+            label1.Text = "Input";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new System.Drawing.Point(257, 44);
+            label2.Name = "label2";
+            label2.Size = new System.Drawing.Size(45, 15);
+            label2.TabIndex = 4;
+            label2.Text = "Output";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new System.Drawing.Point(257, 73);
+            label3.Name = "label3";
+            label3.Size = new System.Drawing.Size(33, 15);
+            label3.TabIndex = 5;
+            label3.Text = "Filter";
+            // 
+            // button1
+            // 
+            button1.Location = new System.Drawing.Point(257, 108);
+            button1.Name = "button1";
+            button1.Size = new System.Drawing.Size(75, 23);
+            button1.TabIndex = 6;
+            button1.Text = "Run";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            ClientSize = new System.Drawing.Size(351, 142);
+            Controls.Add(button1);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(filterTxt);
+            Controls.Add(outPutTxt);
+            Controls.Add(inputTxt);
+            FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox inputTxt;
+        private System.Windows.Forms.TextBox outPutTxt;
+        private System.Windows.Forms.TextBox filterTxt;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ ParagraphSpliter/Form1.cs
@@ -0,0 +1,295 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.LinkLabel;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace ParagraphSpliter
+{
+    public partial class Form1 : Form
+    {
+        List<string> Lines;
+        List<string>[] _words = new List<string>[10];
+        List<string>[] forbbedin = new List<string>[10];
+        List<string> temp;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void inputTxt_DragDrop(object sender, DragEventArgs e)
+        {
+            if (inputTxt.Text != null)
+            {
+                inputTxt.Text = null;
+            }
+            if (e.Data.GetData(DataFormats.FileDrop) is string[] files)
+            {
+                foreach (var file in files)
+                {
+                    inputTxt.Text = file; // Add file paths to the ListBox
+                }
+            }
+        }
+
+        private void inputTxt_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effect = DragDropEffects.Copy; // Show copy cursor
+            }
+            else
+            {
+                e.Effect = DragDropEffects.None; // Invalid drop
+            }
+        }
+
+        private void outPutTxt_DragDrop(object sender, DragEventArgs e)
+        {
+            if (outPutTxt.Text != null)
+            {
+                outPutTxt.Text = null;
+            }
+            if (e.Data.GetData(DataFormats.FileDrop) is string[] files)
+            {
+                foreach (var file in files)
+                {
+                    outPutTxt.Text = file; // Add file paths to the ListBox
+                }
+            }
+        }
+
+        private void outPutTxt_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effect = DragDropEffects.Copy; // Show copy cursor
+            }
+            else
+            {
+                e.Effect = DragDropEffects.None; // Invalid drop
+            }
+        }
+
+        private void filterTxt_DragDrop(object sender, DragEventArgs e)
+        {
+            if (filterTxt.Text != null)
+            {
+                filterTxt.Text = null;
+            }
+            if (e.Data.GetData(DataFormats.FileDrop) is string[] files)
+            {
+                foreach (var file in files)
+                {
+                    filterTxt.Text = file; // Add file paths to the ListBox
+                }
+            }
+        }
+
+        private void filterTxt_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effect = DragDropEffects.Copy; // Show copy cursor
+            }
+            else
+            {
+                e.Effect = DragDropEffects.None; // Invalid drop
+            }
+        }
+
+        private void AddWord()
+        {
+            string[] temp;
+
+            for(int i = 0; i < 10; i++)
+            {
+                _words[i] = new List<string>();
+            }
+
+            for (int i = 0; i < Lines.Count; i++)
+            {
+                temp = Lines[i].Split(" ");
+
+                for (int j = 0; j < temp.Length; j++)
+                {
+
+                    if (temp[j] != "")
+                    {
+                        if (CheckEnglishChar(temp[j][0]))
+                        {
+                            temp[j] = RemoveLastChar(temp[j]);
+
+                            temp[j] = ForceLowerCase(temp[j]);
+
+                            if (Filter(temp[j]))
+                            {
+                                if (DuplicationCheck(temp[j]))
+                                {
+                                    _words[GetHashIndex(temp[j])].Add(temp[j]);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            int wordsCount = 0;
+
+            for(int i = 0; i < 10; i++)
+            {
+                wordsCount += _words[i].Count;
+            }
+
+            MessageBox.Show("Done " + wordsCount + " Counted Word");
+        }
+
+
+        private bool Filter(string s)
+        {
+
+            LoadFilterFile();
+
+            int index = GetHashIndex(s);
+
+            for(int i = 0; i < forbbedin[index].Count; i++)
+            {
+                if (forbbedin[index][i] == s)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        private bool CheckEnglishChar(char c)
+        {
+            return ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'));
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var phrases = File.ReadAllLines(inputTxt.Text);
+                Lines = new List<string>(phrases);
+                AddWord();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("inputTxt File is not exsists");
+            }
+
+            WriteToTxtFile();
+        }
+
+        private string RemoveLastChar(string s)
+        {
+
+            //check for </i>
+
+            if(s[s.Length - 1] == '>' && s[s.Length - 4] == '<')
+            {
+                return RemoveLastChar(s.Substring(0, s.Length - 4));
+            }
+
+            if ((s[s.Length - 1] >= 'A' && s[s.Length - 1] <= 'Z') || (s[s.Length - 1] >= 'a' && s[s.Length - 1] <= 'z'))
+            {
+                return s;
+
+            }
+
+            return RemoveLastChar(s.Substring(0, s.Length - 1));
+        }
+
+        private int GetHashIndex(string input) => input[0] % 10;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void LoadFilterFile()
+        {
+
+            for(int i = 0; i < forbbedin.Length; i++)
+            {
+                forbbedin[i] = new List<string>();
+            }
+
+            try
+            {
+                var forbbed = File.ReadAllLines(filterTxt.Text);
+                temp = new List<string>(forbbed);
+
+                for (int i = 0; i < temp.Count; i++)
+                {
+
+                    forbbedin[GetHashIndex(temp[i])].Add(ForceLowerCase(temp[i]));
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex + "\n _forbbedin List File Not Exestis");
+            }
+        }
+
+        private void WriteToTxtFile()
+        {
+            string filePath = outPutTxt.Text;
+
+            // Write the list of strings to the file
+            for(int i = 0; i < 10; i++)
+            {
+                //File.WriteAllLines(filePath, _words[i]);
+
+                for(int j = 0; j < _words[i].Count; j++)
+                {
+                    File.AppendAllText(filePath, _words[i][j] + Environment.NewLine);
+                }
+            }
+        }
+
+        private bool DuplicationCheck(string input)
+        {
+            int index = GetHashIndex(input);
+
+            for(int i = 0; i < _words[index].Count; i++)
+            {
+                if (_words[index][i] == input)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        private string ForceLowerCase(string input)
+        {
+            char[] chars = input.ToCharArray();
+            int n = chars[0];
+
+            if (n < 90)
+            {
+                n += 32;
+            }
+
+            chars[0] = (char)n;
+
+            return new string(chars);
+        }
+
+    }
+}
